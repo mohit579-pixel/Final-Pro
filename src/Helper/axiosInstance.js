b53a8849fed2b04e8d210724af1a75cc4541e7b6
mohit579-pixel <mohitkhandelwal579@gmail.com>
2025-04-26 10:50:43 +0530

    aadd

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://hospital-bbha.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
